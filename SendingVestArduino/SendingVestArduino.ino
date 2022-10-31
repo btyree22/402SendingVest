debd07fdf583399ef33cd85557ef097f3f06a558
Brooke Tyree <111153510+btyree22@users.noreply.github.com>
2022-10-31 16:20:10 -0500

    Add files via upload
    added 6th button and updated naming

--- SendingVestArduino/SendingVestArduino.ino
+++ SendingVestArduino/SendingVestArduino.ino
@@ -1,17 +1,15 @@
 #include <Arduino.h>
 #include <rf24g.h>
 #include <RF24.h>
-RF24 radio(7, 53);
+RF24 radio(33, 35); // CE, CSN
 const byte address[6] = "00001";
 
 int button_pin_1 = 2;
 int button_pin_2 = 3;
 int button_pin_3 = 4;
-int button_pin_4 = 5;
-int button_pin_5 = 6;
-
-//#define NRF24L01_PIN_CE  7
-//#define NRF24L01_PIN_CS 53
+int button_pin_4 = 8;
+int button_pin_5 = 9;
+int button_pin_6 = 10;
 
 const int timeout = 10000;       //define timeout of 10 sec
 char menuOption = 0;
@@ -22,6 +20,7 @@ boolean button_state_2 = 0;
 boolean button_state_3 = 0;
 boolean button_state_4 = 0;
 boolean button_state_5 = 0;
+boolean button_state_6 = 0;
 
 unsigned long Command;
 
@@ -36,6 +35,7 @@ void setup() {
     pinMode(button_pin_3, INPUT);
     pinMode(button_pin_4, INPUT);
     pinMode(button_pin_5, INPUT);
+    pinMode(button_pin_6, INPUT);
 
     radio.begin();
     radio.openWritingPipe(address);
@@ -51,68 +51,111 @@ void loop() {
   button_state_3 = digitalRead(button_pin_3);
   button_state_4 = digitalRead(button_pin_4);
   button_state_5 = digitalRead(button_pin_5);
+  button_state_6 = digitalRead(button_pin_6);
   
   if(button_state_1 == HIGH && button_state_2 == HIGH){
-    Command = 6;
-    const char text6[] = "Buttons 1 & 2 were pressed";
+    Command = 7;
+    const char text7[] = "Buttons 1 & 2 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 1 & 2 pressed");
   }
 
   else if(button_state_1 == HIGH && button_state_3 == HIGH){
-    Command = 7;
-    const char text7[] = "Buttons 1 & 3 were pressed";
+    Command = 8;
+    const char text8[] = "Buttons 1 & 3 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 1 & 3 pressed");
   }
 
   else if(button_state_1 == HIGH && button_state_4 == HIGH){
-    Command = 8;
-    const char text8[] = "Buttons 1 & 4 were pressed";
+    Command = 9;
+    const char text9[] = "Buttons 1 & 4 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 1 & 4 pressed");
   }
 
   else if(button_state_1 == HIGH && button_state_5 == HIGH){
-    Command = 9;
-    const char text9[] = "Buttons 1 & 5 were pressed";
+    Command = 10;
+    const char text10[] = "Buttons 1 & 5 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 1 & 5 pressed");
   }
 
+  else if(button_state_1 == HIGH && button_state_6 == HIGH){
+    Command = 11;
+    const char text11[] = "Buttons 1 & 6 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 1 & 6 pressed");
+  }
+
   else if(button_state_2 == HIGH && button_state_3 == HIGH){
-    Command = 10;
-    const char text10[] = "Buttons 2 & 3 were pressed";
+    Command = 12;
+    const char text12[] = "Buttons 2 & 3 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 2 & 3 pressed");
   }
 
   else if(button_state_2 == HIGH && button_state_4 == HIGH){
-    Command = 11;
-    const char text11[] = "Buttons 2 & 4 were pressed";
+    Command = 13;
+    const char text13[] = "Buttons 2 & 4 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 2 & 4 pressed");
   }
 
   else if(button_state_2 == HIGH && button_state_5 == HIGH){
-    Command = 12;
-    const char text12[] = "Buttons 2 & 5 were pressed";
+    Command = 14;
+    const char text14[] = "Buttons 2 & 5 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 2 & 5 pressed");
   }
 
+  else if(button_state_2 == HIGH && button_state_6 == HIGH){
+    Command = 15;
+    const char text15[] = "Buttons 2 & 6 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 2 & 6 pressed");
+  }
+
   else if(button_state_3 == HIGH && button_state_4 == HIGH){
-    Command = 13;
-    const char text13[] = "Buttons 3 & 4 were pressed";
+    Command = 16;
+    const char text16[] = "Buttons 3 & 4 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 3 & 4 pressed");
   }
 
+  else if(button_state_3 == HIGH && button_state_5 == HIGH){
+    Command = 17;
+    const char text17[] = "Buttons 3 & 5 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 3 & 5 pressed");
+  }
+
+  else if(button_state_3 == HIGH && button_state_6 == HIGH){
+    Command = 18;
+    const char text18[] = "Buttons 3 & 6 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 3 & 6 pressed");
+  }  
+
   else if(button_state_4 == HIGH && button_state_5 == HIGH){
-    Command = 14;
-    const char text14[] = "Buttons 4 & 5 were pressed";
+    Command = 19;
+    const char text19[] = "Buttons 4 & 5 were pressed";
     radio.write(&Command, sizeof(unsigned long));
     Serial.println("buttons 4 & 5 pressed");
+  }
+
+  else if(button_state_4 == HIGH && button_state_6 == HIGH){
+    Command = 20;
+    const char text20[] = "Buttons 4 & 6 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 4 & 6 pressed");
+  }
+
+  else if(button_state_5 == HIGH && button_state_6 == HIGH){
+    Command = 21;
+    const char text21[] = "Buttons 5 & 6 were pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("buttons 5 & 6 pressed");
   }
 
   else if(button_state_1 == HIGH){
@@ -150,6 +193,12 @@ void loop() {
     Serial.println("button 5 pressed");
   }
   
+  else if(button_state_6 == HIGH){
+    Command = 6;
+    const char text6[] = "Button 6 was pressed";
+    radio.write(&Command, sizeof(unsigned long));
+    Serial.println("button 6 pressed");
+  }
   
   delay(10);
 }
